07e2deb40927ffa98379f87fda27992cfe479970
xuxiaohuan <xu.xiaohuan@dlt-world.com>
2018-10-23 16:41:26 +0800

    蛋糕订单管理

--- src/pages/order/cakeOrders.vue
+++ src/pages/order/cakeOrders.vue
@@ -35,9 +35,9 @@
       <Table border ref="selection" :columns="columns" :data="orderList" :loading="loading"></Table>
     </Row>
     <Row style="margin-top: 10px">
-      <p style="align: center">
+      <Col span="8" offset="8">
         <Page :total="total" :page-size="params.pageSize" size="small" show-elevator show-total @on-change="changePage" @on-page-size-change="changePageSize" />
-      </p>
+      </Col>
     </Row>
   </div>
 </template>
@@ -77,74 +77,144 @@ export default {
       columns: [{
         title: '订单编号',
         sortable: true,
+        align: 'center',
+        width: 120,
         key: 'orderNumber'
       }, {
         title: '交易号',
         sortable: true,
+        align: 'center',
+        width: 120,
         key: 'orderNumber'
       }, {
         title: '下单时间',
         sortable: true,
+        align: 'center',
+        width: 120,
         key: 'orderTime'
       }, {
         title: '商品信息',
-        key: 'name'
+        align: 'center',
+        width: 250,
+        render: (h, params) => {
+          return h('div', {
+            style: {
+              display: 'table-cell',
+              padding: '10px'
+            }
+          }, [
+            h('img', {
+              style: {
+                width: '80px',
+                height: '80px',
+                verticalAlign: 'middle',
+                marginRight: '5px'
+              },
+              attrs: {
+                src: params.row.product[0].imageUrl
+              }
+            }),
+            h('span', params.row.product[0].title)
+          ])
+        }
       }, {
         title: '商品规格',
-        sortable: true
-        // key: 'product'
+        align: 'center',
+        sortable: true,
+        render: (h, params) => {
+          return h('div', {
+            style: {
+              textAlign: 'center'
+            }
+          }, [
+            h('p', params.row.product[0].spec),
+            h('p', params.row.product[0].number + '件')
+          ])
+        }
       }, {
         title: '买家信息',
         sortable: true,
-        key: 'consigneeName'
-        // render: (h, row, col, idx) => {
-        //   return row.consigneeName + '<br>' + row.consigneePhoneNum
-        // }
+        align: 'center',
+        render: (h, params) => {
+          return h('div', {
+            style: {
+              textAlign: 'center'
+            }
+          }, [
+            h('p', params.row.consigneePhoneNum),
+            h('p', params.row.consigneeName)
+          ])
+        }
       }, {
         title: '餐具信息',
-        key: 'name'
+        align: 'center',
+        key: 'name',
+        render: (h, params) => {
+          console.log(params.row)
+          return h('div', {
+            style: {
+              textAlign: 'center'
+            }
+          }, [
+            h('p', '蜡烛：' + params.row.product[0].needCandle),
+            h('p', '餐具：' + params.row.product[0].tablewareNum + '份')
+          ])
+        }
       }, {
         title: '订单金额',
         sortable: true,
+        align: 'center',
         key: 'amount'
       }, {
         title: '订单状态',
         sortable: true,
+        align: 'center',
         key: 'status'
       }, {
         title: '店铺名称',
         sortable: true,
+        align: 'center',
         key: 'shop'
       }, {
         title: '操作',
+        align: 'center',
+        width: 100,
         // key: 'name',
         render: (h, params) => {
-          return h('div', [
-            h('Button', {
-              props: {
-                type: 'success',
-                size: 'small'
-              },
-              style: {
-                marginRight: '5px'
-              },
-              on: {
-                click: () => {
-                  // this.show(params.index)
+          return h('div', {
+            style: {
+              textAlign: 'center'
+            }
+          }, [
+            h('p', [
+              h('Button', {
+                props: {
+                  type: 'success',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    // this.show(params.index)
+                  }
                 }
-              }
-            }, '查看详情'),
-            h('Button', {
-              props: {
-                type: 'success',
-                size: 'small'
-              },
-              on: {
-                click: () => {
-                  // this.remove(params.index)
+              }, '查看详情')
+            ]),
+            h('p', [
+              h('Button', {
+                props: {
+                  type: 'success',
+                  size: 'small'
+                },
+                style: {
+                  marginTop: '5px'
+                },
+                on: {
+                  click: () => {
+                    // this.show(params.index)
+                  }
                 }
-              }
-            }, '确认收货')
+              }, '确认收货')
+            ])
           ])
         }
       }],
